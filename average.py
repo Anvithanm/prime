0f0efe498ab6ea63fe8642d5310eb9d1a739436d
Anvitha <anvitha@anvitha-Vostro-260.(none)>
2017-01-16 16:47:57 +0530

    To find average

---
+++ average.py
@@ -0,0 +1,9 @@
+def avg():
+        list_num = [1,2,3,4,5,6,7]
+        total = sum(list_num)
+        length = len(list_num)
+        average = float(total/length)
+        print average
+avg()
+
+
